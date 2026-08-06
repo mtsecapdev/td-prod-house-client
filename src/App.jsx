db076cbc1ee5125eec8d99403bbb2d97041632e8
lester-mtse-machine <lesterc013@gmail.com>
2026-08-06 10:35:03 +0800

    Render documents as checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,27 @@ function App() {
 
   return (
     <div>
-      <ul>
-        {uploadedDocuments.map((doc) => (
-          <li key={doc.id}>
-            {doc.name} {doc['uploaded_at']}
-          </li>
-        ))}
-      </ul>
+      {error ? (
+        <div>
+          Error: {error.status} {error.message}
+        </div>
+      ) : (
+        <ul>
+          {uploadedDocuments.map((doc) => (
+            // Something a little confusing:
+            // Realise that I use the doc.name as the unique identifier i.e. value of the li key, input id, htmlFor etc.
+            // while I then give the input's 'name' a value of the doc.id.
+            // This is cos I want to send over the doc.id number which the backend will use to select the right documents.
+            // Then because the name is more unique, I use that as the identifier.
+            <li key={doc.name}>
+              <input type='checkbox' name={doc.id} id={doc.name} />
+              <label htmlFor={doc.name}>
+                {doc.name}; Uploaded at {doc['uploaded_at']}
+              </label>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
