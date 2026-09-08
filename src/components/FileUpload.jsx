b42c64454d4dc9f13ed9f519e70a68f71f892a01
mtse <stetcapdev@gmail.com>
2026-09-08 17:05:55 +0800

    fix: Fix that file input did not display 'No file chosen' after file finishes upload
    useRef to directly get a reference to the input. Then when file successfully uploaded, direct DOM manipulation to set the value back to ''.

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import documentsServices from '../services/documentsServices.js';
 
 export default function FileUpload({ setUploadedDocuments }) {
   // TODO: allow upload more than 1 file at a time.
   const [selectedFile, setSelectedFile] = useState(null);
+  const fileInputRef = useRef(null);
 
   async function handleUploadDocument(e) {
     e.preventDefault();
@@ -13,12 +14,15 @@ export default function FileUpload({ setUploadedDocuments }) {
     setUploadedDocuments(await documentsServices.getAllDocumentsAsync());
     // Set selectedFile null
     setSelectedFile(null);
+    // Direct DOM manipulation cos we have a ref to the input to set the value back to null upon successful file upload.
+    fileInputRef.current.value = '';
   }
 
   // Form that allows user to upload a file
   return (
     <form onSubmit={handleUploadDocument}>
       <input
+        ref={fileInputRef}
         type='file'
         onChange={(e) => {
           // Set the selectedFile state
