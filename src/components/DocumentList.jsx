66aeecda9eacc3318af0b6b2ab93b664cac951c1
mtse <stetcapdev@gmail.com>
2026-09-09 15:29:20 +0800

    feat: Implement DocumentList
    This component renders all the uploaded documents as checkboxes. It also controls the states of whether they are selected or not to then be used in the query.

---
+++ src/components/DocumentList.jsx
@@ -0,0 +1,61 @@
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Field,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from '@/components/ui/field';
+import { Button } from '@/components/ui/button';
+import documentsServices from '../services/documentsServices';
+
+/**
+ * Render the document list as checkboxes.
+ * Helps keep track of which documents were selected to be RAG-ed.
+ * @param {Object} param0
+ * @param {{id, name, uploaded_at}[]} param0.uploadedDocuments
+ */
+export default function DocumentList({
+  uploadedDocuments,
+  setUploadedDocuments,
+  setSelectedDocumentIds,
+}) {
+  // Handle the state of selecting of checkbox
+  function handleCheckboxClicked(checked, docId) {
+    setSelectedDocumentIds((prev) =>
+      checked ? [...prev, docId] : prev.filter((id) => id !== docId),
+    );
+  }
+
+  // Handle deleting of documents
+  async function handleDeleteDocument(idToDelete) {
+    await documentsServices.deleteDocumentAsync(idToDelete);
+    setUploadedDocuments(await documentsServices.getAllDocumentsAsync());
+  }
+
+  return (
+    <FieldSet>
+      <FieldLegend variant='label'>
+        Please select a document to query about:
+      </FieldLegend>
+      <FieldGroup className='gap-3'>
+        {uploadedDocuments.map((d) => (
+          <Field orientation='horizontal' key={d.name}>
+            <Checkbox
+              id={d.name}
+              name='document-checkbox'
+              value={d.id}
+              onCheckedChange={(checked) =>
+                handleCheckboxClicked(checked, d.id)
+              }
+            />
+            <FieldLabel htmlFor={d.name} className='font-normal'>
+              {`${d.name}; Uploaded at: ${d.uploaded_at}`}
+            </FieldLabel>
+            <Button onClick={(e) => handleDeleteDocument(d.id)}>Delete</Button>
+          </Field>
+        ))}
+      </FieldGroup>
+    </FieldSet>
+  );
+}
